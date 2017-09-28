e529a5677adcebefe5c107651bbdbfb0b8fdeb2a
BRPickens <brpickens@gmail.com>
2017-09-28 17:18:14 -0500

    Updated to paragraph

--- main.js
+++ main.js
@@ -3,31 +3,32 @@ document.addEventListener('DOMContentLoaded', function() {
  var btn = document.createElement('button');
  var id = document.createElement('sing')
  var div = document.createElement('friend')
- var h3 = document.createElement('myFriends')
+ 
  btn.innerText = 'Sing!';
  document.body.appendChild(btn)
  btn.addEventListener('click', function() {
     for ( d= 0; d < myfriends.length; d++)
-        div =  
-
-
-
-
-
-    for (let i = 0; i < myFriends.length; i++) {
-    console.log(myFriends[i],':');
-    for(var code = 99; code > 0; code--) {
-        if (code ===2) {
-            console.log(code + ' lines of code in the file, ' + code +' lines of code; ' + myFriends[i], ' strikes one out, clears it all out, ' + (code-1) +' line of code in the file');
-        } else if (code === 1) {
-            console.log(code + ' line of code in the file, ' + code + ' line of code;' + myFriends[i], ' strikes one out, clears it all out, no more lines of code in the file');
-        } else  {
-            console.log(code + ' line of code in the file, ' + code + ' line of code;' + myFriends[i], ' strikes one out, clears it all out, ' + (code-1) + ' lines of code in the file');
+        var friend = friends[i];
+        var div = document.createElement('div');
+        var h3 = document.createElement('h3')
+        h3.innerText = 'friend';
+        div.appendChild(h3);
+        var para = document.createElement('p');
+        para.appendChild(); 
+        for (let i = 0; i < myFriends.length; i++) {
+            console.log(myFriends[i],':');
+        for(var code = 99; code > 0; code--) {
+            if (code ===2) {
+                console.log(code + ' lines of code in the file, ' + code +' lines of code; ' + myFriends[i], ' strikes one out, clears it all out, ' + (code-1) +' line of code in the file');
+            } else if (code === 1) {
+                console.log(code + ' line of code in the file, ' + code + ' line of code;' + myFriends[i], ' strikes one out, clears it all out, no more lines of code in the file');
+            } else  {
+                console.log(code + ' line of code in the file, ' + code + ' line of code;' + myFriends[i], ' strikes one out, clears it all out, ' + (code-1) + ' lines of code in the file');
+            }
+    
+        }
         }
     
-    }
-    }
-    
+        })
     })
-})
     
