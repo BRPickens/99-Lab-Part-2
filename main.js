4076dbe4b73424b1a5cdfd751e25e60462ef1cc7
Brandon Pickens <BRPickens@users.noreply.github.com>
2017-09-29 09:31:16 -0500

    added paragraphs

--- main.js
+++ main.js
@@ -1,34 +1,46 @@
-var myFriends = ['Wade', 'Keenan', 'Darcell', 'Andrew', 'Barron']
-document.addEventListener('DOMContentLoaded', function() {
- var btn = document.createElement('button');
- var id = document.createElement('sing')
- var div = document.createElement('friend')
- 
- btn.innerText = 'Sing!';
- document.body.appendChild(btn)
- btn.addEventListener('click', function() {
-    for ( d= 0; d < myfriends.length; d++)
-        var friend = friends[i];
-        var div = document.createElement('div');
-        var h3 = document.createElement('h3')
-        h3.innerText = 'friend';
-        div.appendChild(h3);
-        var para = document.createElement('p');
-        para.appendChild(); 
-        for (let i = 0; i < myFriends.length; i++) {
-            console.log(myFriends[i],':');
-        for(var code = 99; code > 0; code--) {
-            if (code ===2) {
-                console.log(code + ' lines of code in the file, ' + code +' lines of code; ' + myFriends[i], ' strikes one out, clears it all out, ' + (code-1) +' line of code in the file');
-            } else if (code === 1) {
-                console.log(code + ' line of code in the file, ' + code + ' line of code;' + myFriends[i], ' strikes one out, clears it all out, no more lines of code in the file');
-            } else  {
-                console.log(code + ' line of code in the file, ' + code + ' line of code;' + myFriends[i], ' strikes one out, clears it all out, ' + (code-1) + ' lines of code in the file');
+document.addEventListener("DOMContentLoaded", function () {
+    var friends = ["Wade", "Keenan", "Darcell", "Andrew", "Barron"];
+    var buttonContainer = document.createElement('div');
+    var btn = document.createElement('button');
+    var sing = document.createTextNode("Sing!");
+    btn.appendChild(sing);
+    buttonContainer.appendChild(btn);
+    document.body.appendChild(buttonContainer);
+    buttonContainer.id = "button-container";
+    btn.id = "sing-btn";
+    var singBtn = document.getElementById("sing-btn")
+    singBtn.addEventListener("click", function () {
+        for (var i = 0; i < friends.length; i++) {
+            var friendContainer=document.createElement('div');
+            var friendName=document.createElement('h3');
+            var name= document.createTextNode(friends[i]);
+            friendName.appendChild(name);
+            friendContainer.appendChild(friendName);
+            document.body.appendChild(friendContainer);
+           friendContainer.className="friends";
+           friendName.id="name";
+            for (var e = 99; e > 0; e--) {
+                if (e > 2) {                
+                    var para=document.createElement('p');
+                    var song=document.createTextNode(e + " lines of code in the file "+ e+ " lines of code "+ friends[i] + " takes one out, clears it all out, "+ (e - 1) + " lines of code in the file.");
+                    para.appendChild(song);
+                    friendContainer.appendChild(para);                    
+                }
+                else if (e ===2) {
+                    var para=document.createElement('p');
+                    var song=document.createTextNode(e + " lines of code in the file "+ e+ " lines of code "+ friends[i] + " takes one out, clears it all out, "+ (e - 1) + " line of code in the file. ");
+                    para.appendChild(song);
+                    friendContainer.appendChild(para); 
+                }
+                else if (e===1){
+                    var para=document.createElement('p');
+                    var song=document.createTextNode(" 1 line of code in the file, 1 line of code; "+ friends[i] + " takes one out, clears it all out, no more lines of code in the file.");
+                    para.appendChild(song);
+                    friendContainer.appendChild(para);                   
+                }
+
+
             }
-    
         }
-        }
-    
-        })
     })
-    
+})
